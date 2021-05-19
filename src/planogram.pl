a4b5187c13a7152462c3c55a8c72f2ff1dc43832
kaviyachandran <kaviya.dhanabalachandran@gmail.com>
2021-05-19 17:34:43 +0200

    Update according to the new representation

--- src/planogram.pl
+++ src/planogram.pl
@@ -5,28 +5,29 @@
 
 :- use_module(library('model/OWL')).
 
-%% TODO : create planogram instance and assert describes the shop instance with id = store id
-%% store contains shelf instances
-%%
 %%For now, using store id as 1.
 
 create_planogram(StoreId) :-
-    tell(instance_of(StorePlan, shop:'Planogram')),
     get_store_(Store, StoreId),
-    tell(triple(StorePlan, dul:describes, Store)).
+    tell([is_class(Plan),
+        subclass_of(Plan, shop:'Planogram'),
+        instance_of(R, owl:'Restriction'),
+        has_description(R, exactly(soma:isDesignOf, 1, Store)),
+        subclass_of(Plan, R)]).
 
 create_product_type(Name, Gtin, Dimension, Weight, Position, NumberOfFacing, ProductName, StoreId) :-
     [Depth, Width, Height] = Dimension,
     [ShelfId, ShelfLayerId, ProductOrder] = Position,
+    get_store_(Store, StoreId),
     tell([
     	is_class(ProductName),
     	subclass_of(ProductName, shop:'Product'),
-    	is_class(AN),
-    	subclass_of(AN, shop:'ArticleNumber'),
+        % instance of AN
+    	instance_of(AN, shop:'ArticleNumber'),
+        triple(AN, shop:gtin, Gtin),
         instance_of(R, owl:'Restriction'),
     	is_restriction(R,value(shop:articleNumberOfProduct, AN)),
         subclass_of(ProductName, R),
-    	triple(AN, shop:gtin, Gtin),
 
     	has_type(ProductShape, soma:'Shape'),
     	triple(ProductName, soma:hasShape, ProductShape),
@@ -37,118 +38,155 @@ create_product_type(Name, Gtin, Dimension, Weight, Position, NumberOfFacing, Pro
         triple(ProductName, shop:numberOfFacing, NumberOfFacing),
     	has_label(ProductName, Name)]),
     
-    get_shelf_(ShelfId, Shelf),
-    get_shelf_layer_(ShelfLayerId, ShelfLayer),
-    get_store_(Store, StoreId),
+    get_shelf_(ShelfId, Store, Shelf),
+    get_shelf_layer_(ShelfLayerId, Shelf, ShelfLayer),
 
     tell([
-        triple(Store, soma:contains, Shelf),
-        triple(Shelf,soma:hasPhysicalComponent, ShelfLayer),
-        has_type(Label, shop:'ShelfLabel'),
-        triple(ShelfLayer, soma:hasPhysicalComponent, Label),
-        triple(Label, shop:articleNumberOfLabel, AN)
+        % triple(Store, soma:contains, Shelf),
+        % triple(Shelf,soma:hasPhysicalComponent, ShelfLayer),
+        is_class(Label),
+        subclass_of(Label, shop:'ShelfLabel'),
+        is_restriction(R),
+        has_description(R, value(shop:articleNumberOfLabel, AN)),
+        subclass_of(Label, R),
+        is_restriction(R1),
+        has_description(R1, only(dul:isComponentOf, ShelfLayer)),
+        subclass_of(Label, R1)
         ]),
     
     numlist(1, NumberOfFacing, Num),
     forall(member(_, Num),
-            tell([has_type(Facing, shop:'ProductFacingStanding'),
-    	    triple(ProductName, dul:hasLocation, Facing),
-            triple(Facing, shop:layerOfFacing, ShelfLayer)])).
+            tell([ is_class(Facing),
+            subclass_of(Facing, shop:'ProductFacingStanding'),
+            is_restriction(R1),
+            has_description(R1, only(dul:isLocationOf, ProductName)),
+            subclass_of(Facing, R1),
+            is_restriction(R2),
+            has_description(R2, only(shop:layerOfFacing, ShelfLayer)),
+            subclass_of(Facing, R2),
+            is_restriction(R3),
+            has_description(R3, only(shop:labelOfFacing, Label)),
+            subclass_of(Facing, R3)
+    	    % triple(ProductName, dul:hasLocation, Facing),
+            % triple(Facing, shop:layerOfFacing, ShelfLayer)
+            ])).
     
-compute_facing_erp_id(StoreId) :-
-    forall((triple(Store, shop:hasShopId, StoreId),
-        triple(Store, soma:contains, Shelf)),
-        (has_type(Shelf, shop:'ShelfFrame'),
-        get_layers_(Shelf))
-    ).
+% compute_facing_erp_id(StoreId) :-
+%     forall((triple(Store, shop:hasShopId, StoreId),
+%         triple(Store, soma:contains, Shelf)),
+%         (has_type(Shelf, shop:'ShelfFrame'),
+%         get_layers_(Shelf))
+%     ).
 
-get_layers_(Shelf) :-
-    forall(triple(Shelf, soma:hasPhysicalComponent, Layer),
-        (has_type(Layer, shop:'ShelfLayer'),
-        get_order_and_assert_(Layer))
-    ).
+% get_layers_(Shelf) :-
+%     forall(triple(Shelf, soma:hasPhysicalComponent, Layer),
+%         (has_type(Layer, shop:'ShelfLayer'),
+%         get_order_and_assert_(Layer))
+%     ).
 
-get_order_and_assert_(Layer):-
-    findall([Order, Product],
-        (triple(Layer, soma:hasPhysicalComponent, Label),
-        writeln(Label),
-        has_type(Label, shop:'ShelfLabel'),
-        triple(Label, shop:articleNumberOfLabel, AN),
-        writeln(AN), 
-        is_restriction(ANDesc, value(shop:articleNumberOfProduct, AN)),
-        instance_of(ANDesc, owl:'Restriction'),
-        subclass_of(Product, ANDesc),!,
-        writeln([ANDesc, Product]),
-        subclass_of(Product, shop:'Product'),
-        triple(Product, shop:hasOrder, Order),
-        writeln(Order)),
-        OrderAndProduct),
+% get_order_and_assert_(Layer):-
+%     findall([Order, Product],
+%         (triple(Layer, soma:hasPhysicalComponent, Label),
+%         writeln(Label),
+%         has_type(Label, shop:'ShelfLabel'),
+%         triple(Label, shop:articleNumberOfLabel, AN),
+%         writeln(AN), 
+%         is_restriction(ANDesc, value(shop:articleNumberOfProduct, AN)),
+%         instance_of(ANDesc, owl:'Restriction'),
+%         subclass_of(Product, ANDesc),!,
+%         writeln([ANDesc, Product]),
+%         subclass_of(Product, shop:'Product'),
+%         triple(Product, shop:hasOrder, Order),
+%         writeln(Order)),
+%         OrderAndProduct),
     
-    sort(OrderAndProduct, SortedOrder),
-    forall(member([O,P], SortedOrder),
-        (create_facing_id_start_(O, P, FacingIdStart),
-        writeln('All good'),
-        triple(P, shop:numberOfFacing, NumberOfFacing),
-        insert_facing_ids_(P,FacingIdStart, NumberOfFacing))).
+%     sort(OrderAndProduct, SortedOrder),
+%     forall(member([O,P], SortedOrder),
+%         (create_facing_id_start_(O, P, FacingIdStart),
+%         writeln('All good'),
+%         triple(P, shop:numberOfFacing, NumberOfFacing),
+%         insert_facing_ids_(P,FacingIdStart, NumberOfFacing))).
 
-insert_facing_ids_(P, IdStart, 1) :-
-    triple(P, dul:hasLocation, Facing),
-    tell(triple(Facing, shop:erpFacingId, IdStart)).
+% insert_facing_ids_(P, IdStart, 1) :-
+%     triple(P, dul:hasLocation, Facing),
+%     tell(triple(Facing, shop:erpFacingId, IdStart)).
 
-insert_facing_ids_(Product, IdStart, _) :-
-    findall(Facing, 
-        triple(Product, dul:hasLocation, Facing),
-        Facings),
-    tell_facing_id(Facings, IdStart).
+% insert_facing_ids_(Product, IdStart, _) :-
+%     findall(Facing, 
+%         triple(Product, dul:hasLocation, Facing),
+%         Facings),
+%     tell_facing_id(Facings, IdStart).
 
-tell_facing_id([F|Rest], Id) :-
-    tell(triple(F,shop:erpFacingId, Id)),
-    Id1 is Id+1,
-    tell_facing_id(Rest, Id1).
+% tell_facing_id([F|Rest], Id) :-
+%     tell(triple(F,shop:erpFacingId, Id)),
+%     Id1 is Id+1,
+%     tell_facing_id(Rest, Id1).
 
-tell_facing_id([], _).
+% tell_facing_id([], _).
 
-create_facing_id_start_(Order, Product, FacingIdStart) :-
-    \+ Order  is 1.0,
-    numlist(1, Order, PriorOrders),
-    findall(FacingNumber,
-            (   
-                member(ProductOrder, PriorOrders),
-                triple(Product, shop:hasOrder, ProductOrder),
-                triple(Product, shop:numberOfFacing, FacingNumber)
-            ),
-            FacingNumbers),
-    sumlist(FacingNumbers, Temp),
-    FacingIdStart is Temp+1.
+% create_facing_id_start_(Order, Product, FacingIdStart) :-
+%     \+ Order  is 1.0,
+%     numlist(1, Order, PriorOrders),
+%     findall(FacingNumber,
+%             (   
+%                 member(ProductOrder, PriorOrders),
+%                 triple(Product, shop:hasOrder, ProductOrder),
+%                 triple(Product, shop:numberOfFacing, FacingNumber)
+%             ),
+%             FacingNumbers),
+%     sumlist(FacingNumbers, Temp),
+%     FacingIdStart is Temp+1.
 
-create_facing_id_start_(_,_, 1).
+% create_facing_id_start_(_,_, 1).
 
 
-get_shelf_(ShelfId, Shelf) :-
-    triple(Shelf, shop:erpShelfId, ShelfId).
 
-get_shelf_(ShelfId, Shelf) :-
-    tell([ has_type(Shelf, shop:'ShelfFrame'),
-            triple(Shelf, shop:erpShelfId, ShelfId)
+get_shelf_(ShelfId, Store, Shelf) :-
+    has_description(R, value(shop:erpShelfId, ShelfId)),
+    subclass_of(ShelfFrame, R),
+    subclass_of(ShelfFrame, R1),
+    has_description(R1, only(soma:isContainedIn, Store)),
+    !.
+
+get_shelf_(ShelfId, Store, Shelf) :-
+    tell([  is_class(ShelfFrame),
+            subclass_of(ShelfFrame, shop:'ShelfFrame'),
+            is_restriction(R),
+            has_description(R, value(shop:erpShelfId, ShelfId)),
+            subclass_of(ShelfFrame, R),
+            is_restriction(R1),
+            has_description(R1, only(soma:isContainedIn, Store)),
+            subclass_of(ShelfFrame, R1)
         ]).
 
-get_shelf_layer_(Id, Obj) :-
-    triple(Obj, shop:erpShelfLayerId, Id).
+get_shelf_layer_(Id, Shelf, Obj) :-
+     has_description(R, value(shop:erpShelfLayerId, ShelfId)),
+    subclass_of(ShelfFrame, R),
+    subclass_of(ShelfFrame, R1),
+    has_description(R1, only(dul:isComponentOf, Store)),!.
 
-get_shelf_layer_(Id, Obj) :-
-    tell([ has_type(Obj, shop:'ShelfLayer'),
-            triple(Obj, shop:erpShelfLayerId, Id)
+get_shelf_layer_(Id, Shelf, Obj) :-
+    tell([  is_class(Obj),
+            subclass_of(Obj, shop:'ShelfLayer'),
+            is_restriction(R),
+            has_description(R, value(shop:erpShelfLayerId, Id)),
+            is_restriction(R1),
+            has_description(R1, only(shop:isComponentOf, Shelf)),
+            subclass_of(Obj, R1),
+            subclass_of(Obj, R)
         ]).
 
 get_store_(Store, StoreId) :-
-    triple(Store, shop:hasShopId, StoreId).
+    has_description(RId, value(shop:hasShopId, StoreId)),
+    subclass_of(Store, RId), !.
 
 get_store_(Store, StoreId) :-
-    tell([  has_type(Store, shop:'Shop'),
-            triple(Store, shop:hasShopId, StoreId)
+    tell([  is_class(Store),
+            subclass_of(Store, shop:'Shop'),
+            is_restriction(RId),
+            has_description(RId, value(shop:hasShopId, StoreId)),
+            subclass_of(Store, RId)
         ]).
-
-
 
 
 
