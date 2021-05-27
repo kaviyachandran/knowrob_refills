0107a0f7d743a9ec177e5019b85089a83197792d
¨kaviyachandran¨ <kaviya.dhanabalachandran@gmail.com>
2021-05-27 17:20:10 +0200

    Added predicates to create realogram data for testing

--- src/planogram.pl
+++ src/planogram.pl
@@ -12,7 +12,7 @@ create_planogram(StoreId) :-
     tell([is_class(Plan),
         subclass_of(Plan, shop:'Planogram'),
         instance_of(R, owl:'Restriction'),
-        has_description(R, exactly(soma:isDesignOf, 1, Store)),
+        is_restriction(R, exactly(soma:isDesignOf, 1, Store)),
         subclass_of(Plan, R)]).
 
 create_product_type(Name, Gtin, Dimension, Weight, Position, NumberOfFacing, ProductName, StoreId) :-
@@ -174,7 +174,7 @@ get_shelf_layer_(Id, Shelf, ShelfLayer) :-
             instance_of(R, owl:'Restriction'),
             is_restriction(R, value(shop:erpShelfLayerId, Id)),
             instance_of(R1, owl:'Restriction'),
-            is_restriction(R1, only(shop:isComponentOf, Shelf)),
+            is_restriction(R1, only(dul:isComponentOf, Shelf)),
             subclass_of(ShelfLayer, R1),
             subclass_of(ShelfLayer, R)
         ]).
@@ -194,7 +194,38 @@ get_store_(Store, StoreId) :-
 
 
 
+shelf_individual_of(Shelf, ShelfClass) :-
+    var(ShelfClass), !,
+    
+    triple(Shelf, shop:erpShelfId, Id),
+    triple(Store, soma:containsObject, Shelf),
+    triple(Store, shop:hasShopId, ShopId),
 
+    get_store_(StorePlan, ShopId),
+    get_shelf_(Id, StorePlan, ShelfClass),
+
+    get_all_shelf_layers_(ShelfClass, LayerClasses),
+
+    %%% Check if the components satisfy 
+    findall(LayerInstance, 
+            (triple(Shelf, soma:hasPhysicalComponent, LayerInstance),
+            layer_instance_of(LayerInstance, ShelfClass,LayerClass)),
+            LayerInstances).
+
+
+layer_instance_of(Layer, SClass, LClass) :-
+    triple(Layer, shop:erpShelfLayerId, Id),
+    get_shelf_layer_(Id, SClass, LClass),
+
+    %%% Check the parts of the shelf
+    
+
+
+get_all_shelf_layers_(Shelf, Ls) :-
+    findall(L, 
+        (is_restriction(R1, only(dul:isComponentOf, Shelf)),
+        subclass_of(L, R1)), 
+        Ls).
 
 %%%%%%%%%%%%%% Reasonign about differences
 
@@ -218,17 +249,61 @@ elemcmp(A,B) :- var(A), var(B), ! ; A =@= B.
 
 /* compare_list(A, B) :-
     maplist(elemcmp,A, B). */
-    
 
-    
+create_realo_store(StoreNum, Store) :-
+    tell([has_type(Store, shop:'Shop'),
+        triple(Store, shop:hasShopId, StoreNum)]).
+
+create_realogram_test_shelf(Store, ShelfNum, ShelfLayerNum, ProdId, NoOfFacings) :-
+    get_shelfR(ShelfNum, Shelf),
+    get_shelflayerR(ShelfLayerNum, Layer),
+
+    tell([ instance_of(AN, shop:'ArticleNumber'),
+        triple(AN, shop:gtin, ProdId),
+        instance_of(R, owl:'Restriction'),
+        is_restriction(R, value(shop:articleNumberOfProduct, AN)),
+        is_class(P),
+    	subclass_of(P, shop:'Product'),
+        subclass_of(P, R),
+
+        has_type(Label, shop:'ShelfLabel'),
+        triple(Label,shop:articleNumberOfLabel, AN)
+        ]),
+    numlist(1, NoOfFacings, Num),
+    forall(member(_, Num),
+            tell([ has_type(Facing, shop:'ProductFacingStanding'),
+                triple(Facing, shop:labelOfFacing, Label),
+                triple(Facing, shop:productLabelOfFacing, P)
+                ])).
+
+get_shelfR(Num, Shelf):-
+    triple(Shelf, shop:erpShelfId, Num);
+    tell([ has_type(Shelf, shop:'ShelfFrame'),
+        triple(Shelf, shop:erpShelfId, Num)]).
+
+get_shelflayerR(Id, L):-
+    triple(L, shop:erpShelfLayerId, Id);
+    tell([ has_type(L, shop:'ShelfLayer'),
+        triple(L, shop:erpShelfLayerId, Id)]).
+
+get_product_and_art_num(ProdId, P, AN) :-
+    (is_restriction(R, value(shop:articleNumberOfProduct, AN)),
+    triple(AN, shop:gtin, ProdId),
+    subclass_of(P, R));
+    ()
 
 :- begin_tests(planogram).
+
+
+
 
 test('product') :-
     writeln('I am here'),
     gtrace,
-    create_product_type('shampoo', 45344545, [0.8, 0.2, 0.1], 2.3, [17, 5, 1], 2, _, 1).
-    % create_product_type('soap', 453444563, [0.8, 0.2, 0.1], 2.3, [17, 5, 2], 3, _, 1),
+    create_product_type('shampoo', 45344545, [0.8, 0.2, 0.1], 2.3, [17, 5, 1], 2, _, 100),
+    create_product_type('soap1', 453444563, [0.8, 0.2, 0.1], 2.3, [17, 5, 2], 3, _, 100),
+    create_product_type('soap2', 45347565563, [0.8, 0.2, 0.1], 2.3, [17, 5, 3], 1, _, 100),
+    create_product_type('soap3', 45354756563, [0.8, 0.2, 0.1], 2.3, [17, 5, 4], 1, _, 100).
     % compute_facing_erp_id(1).
 
 /* test('recursion') :-
